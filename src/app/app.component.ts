93cfbc68fa142363febfd4e72e686cb9c1f7ae7c
Kuroshin28 <baptiste.bertinWOT@gmail.com>
2023-05-25 15:38:12 +0200

    Add requires data for app components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Administrate';
+
+  adminAuthorisation : boolean = false;
+
+  logAdmin() {
+    this.adminAuthorisation = !this.adminAuthorisation;
+  }
 }
